e774634d2a6dc0c6b05d0bff49ecadd1491951f8
jwesley <jwesley@43ce8cbc-7bfa-0310-8849-d623577eb12c>
2004-09-06 14:57:49 +0000

    mapping table setup

--- trunk/JPAS/src/org/jpas/da/DBNames.java
+++ trunk/JPAS/src/org/jpas/da/DBNames.java
@@ -11,43 +11,45 @@ package org.jpas.da;
 
 public class DBNames
 {
-	public static String TN_ACCOUNT = "tbl_account";
+	public static final String TN_TRANSACTION = "transaction_tbl";
+	public static final String CN_TRANSACTION_ID = "id";
+	public static final String CN_TRANSACTION_PAYEE = "payee";
+	public static final String CN_TRANSACTION_ACCOUNT = "account";
+	public static final String CN_TRANSACTION_DATE = "date";
+	public static final String CN_TRANSACTION_NUM = "num";
+	public static final String CN_TRANSACTION_MEMO = "memo";
+	public static final String IDX_TRANSACTION_ACCOUNT = "trans_account_idx";
+	public static final String SEQ_TRANSACTION_ID = "transaction_seq";
+
+	public static final String TN_TRANSACTION_ACCOUNT_MAP = "trans_account_map_tbl";
+	public static final String CN_TAM_ACCOUNT_ID = "account_id";
+	public static final String CN_TAM_TRANSACTION_ID = "transaction_id";
+	public static final String CN_TAM_AMOUNT = "amount";
+	public static final String IDX_TAM_ACCOUNT = "tam_account_idx";
+	public static final String IDX_TAM_TRANSACTION = "tam_transaction_idx";
+		
+	public static String TN_ACCOUNT = "account_tbl";
 	public static String CN_ACCOUNT_ID = "id";
 	public static String CN_ACCOUNT_NAME = "name";
-	public static String SEQ_ACCOUNT_ID = "seq_account_id";
+	public static String CN_ACCOUNT_PRIMARY = "bank";
+	public static String SEQ_ACCOUNT_ID = "account_seq";
 
+	public static String TN_REMINDER_ACCOUNT_MAP = "ram_account_map_tbl";
+	public static String CN_RAM_ACCOUNT_ID = "account_id";
+	public static String CN_RAM_REMINDER_ID = "reminder_id";
+	public static String CN_RAM_AMOUNT = "amount";
+	public static final String IDX_RAM_ACCOUNT = "ram_account_idx";
+	public static final String IDX_RAM_REMINDER = "ram_reminder_idx";
 
-	public static String TN_TRANSACTION = "tbl_transaction";
-	public static String CN_TRANSACTION_ID = "id";
-	public static String CN_TRANSACTION_PAYEE = "payee";
-	public static String CN_TRANSACTION_ACCOUNT = "account";
-	public static String CN_TRANSACTION_AMOUNT = "amount";
-	public static String CN_TRANSACTION_DATE = "date";
-	public static String CN_TRANSACTION_NUM = "num";
-	public static String CN_TRANSACTION_MEMO = "memo";
-	public static String CN_TRANSACTION_OPPOSITE = "opposite";
-	public static String IDX_TRANSACTION_ACCOUNT = "idx_transaction_account";
-	public static String IDX_TRANSACTION_PAYEE = "idx_transaction_payee";
-	public static String SEQ_TRANSACTION_ID = "seq_transaction_id";
-
-
-	public static String TN_CATEGORY = "tbl_category";
-	public static String CN_CATEGORY_ID = "id";
-	public static String CN_CATEGORY_NAME = "name";
-	public static String CN_CATEGORY_PARENT = "parent";
-	public static String CN_CATEGORY_TAX_DEDUCTABLE = "tax_deductable";
-	public static String IDX_CATEGORY_NAME = "idx_category_name";
-	public static String SEQ_CATEGORY_ID = "seq_category_id";
-
-	public static String TN_REMINDER = "tbl_reminder";
+	
+	public static String TN_REMINDER = "reminder_tbl";
 	public static String CN_REMINDER_ID = "id";
 	public static String CN_REMINDER_PAYEE = "payee";
+	public static String CN_REMINDER_DATE = "date";
 	public static String CN_REMINDER_ACCOUNT = "account";
 	public static String CN_REMINDER_AMOUNT_METHOD = "amount_method";
-	public static String CN_REMINDER_AMOUNT_VALUE = "amount_value";
 	public static String CN_REMINDER_REPEAT_METHOD = "repeat_method";
 	public static String CN_REMINDER_REPEAT_VALUE = "repeat_value";
-	public static String CN_REMINDER_CATEGORY = "category";
 	public static String CN_REMINDER_MEMO = "memo";
-	public static String SEQ_REMINDER_ID = "seq_reminder_id";
+	public static String SEQ_REMINDER_ID = "reminder_seq";
 }
