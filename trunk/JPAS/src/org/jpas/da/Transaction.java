c70dd3466c8cc19d63fa86f480809eaa641e27c8
jwesley <jwesley@43ce8cbc-7bfa-0310-8849-d623577eb12c>
2004-09-11 00:53:12 +0000

    *** empty log message ***

---
+++ trunk/JPAS/src/org/jpas/da/Transaction.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Sep 8, 2004 - 7:08:04 PM
+ * 
+ * Title: JPAS
+ * Description: Java based Personal Accounting System
+ * Copyright: Copyright (c) 2004
+ * License: Distributed under the terms of the GPL v2
+ * @author Justin Smith
+ * @version 1.0
+ */
+package org.jpas.da;
+
+/**
+ * @author Owner
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Transaction
+{
+    public static void main(String[] args)
+    {
+    }
+}
