a9c69ea9dc71ad0764350ed122edce6e338ac3a0
jwesley <jwesley@43ce8cbc-7bfa-0310-8849-d623577eb12c>
2004-10-03 04:07:38 +0000

    Changed File Header

--- trunk/JPAS/src/org/jpas/da/InitDB.java
+++ trunk/JPAS/src/org/jpas/da/InitDB.java
@@ -4,12 +4,25 @@ import java.sql.*;
 import org.apache.log4j.*;
 
 /**
- * <p>Title: JPAS</p>
- * <p>Description: Java based Personal Accounting System</p>
- * <p>Copyright: Copyright (c) 2004</p>
- * <p>License: Distributed under the terms of the GPL v2</p>
- * @author Justin Smith
+ * Title: JPAS
+ * Description: Java based Personal Accounting System
+ * Copyright: Copyright (c) 2004 Justin W Smith
+ * @author Justin W Smith
  * @version 1.0
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 public class InitDB
--- trunk/JPAS/src/org/jpas/da/TransAccountMappingDA.java
+++ trunk/JPAS/src/org/jpas/da/TransAccountMappingDA.java
@@ -9,11 +9,25 @@ import org.apache.log4j.*;
 /**
  * Created on Sep 11, 2004 - 9:59:59 AM
  * 
- * Title: JPAS Description: Java based Personal Accounting System Copyright:
- * Copyright (c) 2004 License: Distributed under the terms of the GPL v2
- * 
- * @author Justin Smith
+ * Title: JPAS
+ * Description: Java based Personal Accounting System
+ * Copyright: Copyright (c) 2004 Justin W Smith
+ * @author Justin W Smith
  * @version 1.0
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 public class TransAccountMappingDA
 {
