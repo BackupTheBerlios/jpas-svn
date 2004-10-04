0c9871b364e58cc7d3fcb2cae6154c882e6f2f98
jwesley <jwesley@43ce8cbc-7bfa-0310-8849-d623577eb12c>
2004-10-04 20:48:23 +0000

    An Observable which will be extended by the models

---
+++ trunk/JPAS/src/org/jpas/util/JpasObservable.java
@@ -0,0 +1,86 @@
+/**
+ * Created on Oct 4, 2004
+ *
+ * Title: JPAS
+ * Description: Java based Personal Accounting System
+ * Copyright: Copyright (c) 2004 Justin W Smith
+ * @author Justin W Smith
+ * @version 1.0
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.jpas.util;
+
+import java.lang.ref.WeakReference;
+import java.util.*;
+
+/**
+ * @author jsmith
+ *
+ */
+public class JpasObservable<V>
+{
+	private final Set<WeakReference<JpasObserver>> observerSet = new HashSet<WeakReference<JpasObserver>>();
+	
+	public void addObserver(final JpasObserver<V> o)
+	{
+		synchronized(observerSet)
+		{
+			observerSet.add(new WeakReference<JpasObserver>(o));
+		}
+	}
+	
+	public void deleteObserver(final JpasObserver<V> o)
+	{
+		synchronized(observerSet)
+		{
+			observerSet.remove(o);
+		}
+	}
+	
+	public void notifyObservers(JpasDataChange<V> arg)
+	{
+		synchronized(observerSet)
+		{
+			for(WeakReference<JpasObserver> wr : observerSet)
+			{
+				final JpasObserver ob = wr.get();
+				if(ob == null)
+				{
+					observerSet.remove(wr);
+					continue;
+				}
+				
+				ob.update(this, arg);
+			}
+		}
+	}
+	
+	protected void deleteObservers()
+	{
+		synchronized(observerSet)
+		{
+			observerSet.clear();
+		}
+	}
+	
+	public int countObservers()
+	{
+		synchronized(observerSet)
+		{
+			return observerSet.size();
+		}
+	}
+}
