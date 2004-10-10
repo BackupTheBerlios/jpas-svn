87b370e93cfae6fca2280eb18ca22d9db2461ce0
jwesley <jwesley@43ce8cbc-7bfa-0310-8849-d623577eb12c>
2004-10-10 20:45:36 +0000

    all values should be final

--- trunk/JPAS/src/org/jpas/da/DBNames.java
+++ trunk/JPAS/src/org/jpas/da/DBNames.java
@@ -41,28 +41,28 @@ public class DBNames
 	public static final String IDX_TAM_ACCOUNT = "tam_account_idx";
 	public static final String IDX_TAM_TRANSACTION = "tam_transaction_idx";
 		
-	public static String TN_ACCOUNT = "account_tbl";
-	public static String CN_ACCOUNT_ID = "id";
-	public static String CN_ACCOUNT_NAME = "name";
-	public static String CN_ACCOUNT_TYPE = "type";
-	public static String SEQ_ACCOUNT_ID = "account_seq";
+	public static final String TN_ACCOUNT = "account_tbl";
+	public static final String CN_ACCOUNT_ID = "id";
+	public static final String CN_ACCOUNT_NAME = "name";
+	public static final String CN_ACCOUNT_TYPE = "type";
+	public static final String SEQ_ACCOUNT_ID = "account_seq";
 
-	public static String TN_REMINDER_ACCOUNT_MAP = "rem_account_map_tbl";
-	public static String CN_RAM_ACCOUNT_ID = "account_id";
-	public static String CN_RAM_REMINDER_ID = "reminder_id";
-	public static String CN_RAM_AMOUNT = "amount";
+	public static final String TN_REMINDER_ACCOUNT_MAP = "rem_account_map_tbl";
+	public static final String CN_RAM_ACCOUNT_ID = "account_id";
+	public static final String CN_RAM_REMINDER_ID = "reminder_id";
+	public static final String CN_RAM_AMOUNT = "amount";
 	public static final String IDX_RAM_ACCOUNT = "ram_account_idx";
 	public static final String IDX_RAM_REMINDER = "ram_reminder_idx";
 
 	
-	public static String TN_REMINDER = "reminder_tbl";
-	public static String CN_REMINDER_ID = "id";
-	public static String CN_REMINDER_PAYEE = "payee";
-	public static String CN_REMINDER_DATE = "date";
-	public static String CN_REMINDER_ACCOUNT = "account";
-	public static String CN_REMINDER_AMOUNT_METHOD = "amount_method";
-	public static String CN_REMINDER_REPEAT_METHOD = "repeat_method";
-	public static String CN_REMINDER_REPEAT_VALUE = "repeat_value";
-	public static String CN_REMINDER_MEMO = "memo";
-	public static String SEQ_REMINDER_ID = "reminder_seq";
+	public static final String TN_REMINDER = "reminder_tbl";
+	public static final String CN_REMINDER_ID = "id";
+	public static final String CN_REMINDER_PAYEE = "payee";
+	public static final String CN_REMINDER_DATE = "date";
+	public static final String CN_REMINDER_ACCOUNT = "account";
+	public static final String CN_REMINDER_AMOUNT_METHOD = "amount_method";
+	public static final String CN_REMINDER_REPEAT_METHOD = "repeat_method";
+	public static final String CN_REMINDER_REPEAT_VALUE = "repeat_value";
+	public static final String CN_REMINDER_MEMO = "memo";
+	public static final String SEQ_REMINDER_ID = "reminder_seq";
 }
