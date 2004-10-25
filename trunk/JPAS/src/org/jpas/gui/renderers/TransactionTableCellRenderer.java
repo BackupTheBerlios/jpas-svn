6333cb0b37db0da4211417933d4ec8854b057e49
jwesley <jwesley@43ce8cbc-7bfa-0310-8849-d623577eb12c>
2004-10-25 22:13:40 +0000

    rewrite

--- trunk/JPAS/src/org/jpas/gui/renderers/TransactionTableCellRenderer.java
+++ trunk/JPAS/src/org/jpas/gui/renderers/TransactionTableCellRenderer.java
@@ -28,13 +28,24 @@ import javax.swing.*;
 import java.awt.*;
 import org.jpas.model.*;
 
+import com.sun.org.apache.bcel.internal.generic.BALOAD;
+import com.toedter.calendar.*;
+
 /**
  * @author Justin W Smith
  *
  */
 public class TransactionTableCellRenderer extends JPanel implements TableCellRenderer
 {
-    final JLabel label = new JLabel(" ");
+    final JDateChooser dateChooser = new JDateChooser();
+	final JComboBox numList = new JComboBox(new String[]{"TXFR", "ATM", "100"});
+    final JComboBox payeeList = new JComboBox(new String[]{"Payee1", "Payee2"});
+    final JTextField withdrawField = new JTextField();
+    final JTextField depositField = new JTextField();
+    final JLabel balanceLabel = new JLabel();
+    final JComboBox categoryList = new JComboBox(new String[]{"cat1", "cat2"});
+	
+	final JLabel label = new JLabel(" ");
     
     /**
      * 
@@ -42,9 +53,72 @@ public class TransactionTableCellRenderer extends JPanel implements TableCellRen
     public TransactionTableCellRenderer()
     {
         this.setOpaque(true);
-        this.setBackground(Color.green);
-        this.add(label);
+        this.setBackground(Color.white);
+        setLayout(new GridBagLayout());
+        init();
     }
+    
+    private void init()
+    {
+    	final GridBagConstraints gbc = new GridBagConstraints();
+    	gbc.fill = GridBagConstraints.BOTH;
+    	gbc.gridx = 0;
+    	gbc.gridy = 0;
+    	
+    	add(dateChooser, gbc);
+    	gbc.gridx++;
+    	
+    	add(numList, gbc);
+    	gbc.gridx++;
+    	gbc.gridwidth = 2;
+    	gbc.weightx = 1;
+    	
+    	add(payeeList, gbc);
+    	gbc.gridx++;
+    	gbc.gridwidth = 1;
+    	gbc.weightx = 0;
+    	
+    	add(withdrawField, gbc);
+    	gbc.gridx++;
+    	
+    	add(depositField, gbc);
+    	gbc.gridx++;
+    	
+    	add(createEmptyPanel(), gbc);
+    	gbc.gridx = 0;
+    	gbc.gridy++;
+    	
+    	add(createEmptyPanel(), gbc);
+    	gbc.gridx++;
+    	
+    	add(createEmptyPanel(), gbc);
+    	gbc.gridx++;
+
+    	add(categoryList, gbc);
+    	gbc.gridx++;
+    	gbc.weightx = 1;
+    	
+    	add(createEmptyPanel(), gbc);
+    	gbc.gridx++;
+    	gbc.weightx = 0;
+
+    	add(createEmptyPanel(), gbc);
+    	gbc.gridx++;
+
+    	add(createEmptyPanel(), gbc);
+    	gbc.gridx++;
+
+    	add(balanceLabel, gbc);
+    	gbc.gridx++;
+}
+    
+    private JPanel createEmptyPanel()
+    {
+    	final JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createLineBorder(Color.gray));
+    	return panel;
+    }
+    
 
     public Component getTableCellRendererComponent(JTable table,
             Object value,
@@ -55,11 +129,11 @@ public class TransactionTableCellRenderer extends JPanel implements TableCellRen
     {
         if(value == null)
         {
-            label.setText(" ");
+//            label.setText(" ");
         }
         else
         {
-            label.setText(((Transaction)value).getPayee());
+            //label.setText(((Transaction)value).getPayee());
         }
         return this;
     }
