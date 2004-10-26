8b3276c7af88f41af9c835040b168bc996f5a91f
jwesley <jwesley@43ce8cbc-7bfa-0310-8849-d623577eb12c>
2004-10-26 17:10:43 +0000

    Created separate editor for transaction table

---
+++ trunk/JPAS/src/org/jpas/gui/editors/TransactionTableCellEditor.java
@@ -0,0 +1,133 @@
+/*
+ * Created on Oct 26, 2004
+ *
+ * Title: JPAS
+ * Description: Java based Personal Accounting System
+ * Copyright: Copyright (c) 2004 Justin W Smith
+ * @author Justin W Smith
+ * @version 1.0
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.jpas.gui.editors;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.TableCellEditor;
+
+import org.jpas.gui.layouts.FlexGridLayout;
+
+import com.toedter.calendar.JDateChooser;
+
+/**
+ * @author jsmith
+ *
+ */
+public class TransactionTableCellEditor extends AbstractCellEditor implements TableCellEditor {
+
+    final JPanel cellPanel = new JPanel();
+    
+    final JDateChooser dateChooser = new JDateChooser();
+	final JComboBox numList = new JComboBox(new String[]{"TXFR", "ATM", "100"});
+    final JComboBox payeeList = new JComboBox(new String[]{"Payee1", "Payee2"});
+    final JTextField withdrawField = new JTextField("0000.00", 8);
+    final JTextField depositField = new JTextField("0000.00", 8);
+    final JLabel balanceLabel = new JLabel("0000.00");
+    final JComboBox categoryList = new JComboBox(new String[]{"cat1", "cat2"});
+	
+	final JLabel label = new JLabel(" ");
+
+    /**
+     * 
+     */
+    public TransactionTableCellEditor()
+    {
+        cellPanel.setOpaque(true);
+        cellPanel.setBackground(Color.white);
+        init();
+    }
+    
+    private void init()
+    {
+        final FlexGridLayout layout = new FlexGridLayout(new int[]{18, 18}, new int[]{85, 85, 105, 85, 85, 95});
+        layout.setFlexColumn(2, true);
+        cellPanel.setLayout(layout);
+    	cellPanel.add(dateChooser);
+    	cellPanel.add(numList);
+    	cellPanel.add(payeeList);
+    	cellPanel.add(withdrawField);
+    	cellPanel.add(depositField);
+    	cellPanel.add(createEmptyPanel());
+    	cellPanel.add(createEmptyPanel());
+    	cellPanel.add(createEmptyPanel());
+    	cellPanel.add(categoryList);
+    	cellPanel.add(createEmptyPanel());
+    	cellPanel.add(createEmptyPanel());
+//    	cellPanel.add(createEmptyPanel());
+    	cellPanel.add(balanceLabel);
+}
+    
+    private JPanel createEmptyPanel()
+    {
+    	final JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createLineBorder(Color.gray));
+    	return panel;
+    }
+    
+
+    public Component getTableCellRendererComponent(JTable table,
+            Object value,
+            boolean isSelected,
+            boolean hasFocus,
+            int row,
+            int column)
+    {
+        if(value == null)
+        {
+//            label.setText(" ");
+        }
+        else
+        {
+            //label.setText(((Transaction)value).getPayee());
+        }
+        return cellPanel;
+    }
+    
+    public Component getTableCellEditorComponent(JTable table,
+            Object value,
+            boolean isSelected,
+            int row,
+            int column)
+    {
+        return cellPanel;
+    }
+    
+    public Object getCellEditorValue()
+    {
+        return null;
+    }
+    
+    public static void main(String[] args)
+    {
+    }
+}
