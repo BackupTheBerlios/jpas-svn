321aa113b96e8993ba20466e91af63960571bf33
jwesley <jwesley@43ce8cbc-7bfa-0310-8849-d623577eb12c>
2005-06-16 02:37:02 +0000

    Added more test transactions

--- trunk/JPAS/test.sql
+++ trunk/JPAS/test.sql
@@ -4,4 +4,6 @@ select * from transaction_tbl;
 
 select * from trans_account_map_tbl;
 
-SELECT SUM(trans_account_map_tbl.amount) FROM trans_account_map_tbl , transaction_tbl WHERE transaction_tbl.id = trans_account_map_tbl.transaction_id AND transaction_tbl.account = 2
+SELECT SUM(trans_account_map_tbl.amount) FROM trans_account_map_tbl , transaction_tbl WHERE transaction_tbl.id = trans_account_map_tbl.transaction_id AND transaction_tbl.account = 2;
+
+SELECT transaction_tbl.id, transaction_tbl.date FROM transaction_tbl , account_tbl , trans_account_map_tbl WHERE trans_account_map_tbl.transaction_id = transaction_tbl.id AND trans_account_map_tbl.account_id = account_tbl.id AND account_tbl.id = 2 UNION  SELECT id, date FROM transaction_tbl WHERE account = 2 ORDER BY transaction_tbl.date, transaction_tbl.id;
